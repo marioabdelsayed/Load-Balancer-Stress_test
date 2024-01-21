cf31bf1fe6b2620ffe7ff37a929db46186445f94
Mario Abdelsayed <111323045+marioabdelsayed@users.noreply.github.com>
2024-01-21 14:06:26 -0500

    Update serve.py

--- serve.py
+++ serve.py
@@ -9,6 +9,7 @@ def seedFunc():
         return socket.gethostname();
     if request.method == 'POST':
         stress_test = subprocess.Popen(["sudo","python3","stress_cpu.py"]);
+        return str(stress_test)
 
 if __name__ == "__main__":
     app.run()
